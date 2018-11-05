8ba6e1ad87d2e5432e6b5f491f10e9e39f4f8f80
niklasehinger <niklasehinger@googlemail.com>
2018-11-05 19:10:02 +0100

    hey

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
 <html lang="de">
 
 <head>
-    <title>Pigeon</title>
+    <title>Pigeon1</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
