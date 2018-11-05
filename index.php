ffc3985801f8efcecfb2e6a2581c5dd701490988
niklasehinger <niklasehinger@googlemail.com>
2018-11-05 19:00:55 +0100

    hey

--- index.php
+++ index.php
@@ -1,18 +1,43 @@
 
+<?php
+require '../passwords/db.php';
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 
 <head>
+    <title>Pigeon</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="style.css" >
 
-    <title>Pigeon inc.</title>
 </head>
 
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    if (isset($_POST['login'])) {
+        require 'login.php';
+    }
+
+    elseif (isset($_POST['register'])) {
+        require 'register.php';
+    }
+}
+?>
+
 <body>
+
+    <div class="form">
+
+    </div>
+
+
+
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
---
+++ register.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niklasehinger
+ * Date: 05.11.18
+ * Time: 18:57
+ */
