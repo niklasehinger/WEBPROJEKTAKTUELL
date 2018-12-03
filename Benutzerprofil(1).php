f58440622022deca38127e21227b414dfa6d083d
maraa317 <40471341+pigspickausername@users.noreply.github.com>
2018-12-03 15:11:46 +0100

    Angaben im Profil ändern

--- Benutzerprofil(1).php
+++ Benutzerprofil(1).php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 if(!isset($_SESSION['username'])) {
-    die("Bitte erst einloggen"); //Mit die beenden wir den weiteren Scriptablauf
+    echo "kuzfk";
+    echo"Bitte zuerst <a href=\"login.php\">einloggen</a>";
+    die();
 }
+
 
 $name = $_SESSION['username'];
 
@@ -16,17 +19,12 @@ session_start();
 
 $fakultät = $_POST["fakultät"];
 $studiengang = $_POST["studiengang"];
-$studiengang1 = $_POST["studiengang1"];
-$studiengang2 = $_POST["studiengang2"];
+
 $semester = $_POST["semester"];
-$projekte = $_POST["projekte"];
 
 $arbeitgeber = $_POST["arbeitgeber"];
-$schule = $_POST["schule"];
-$seit = $_POST["seit"];
 
-$websites = $_POST["websites"];
-$sociallinks = $_POST["sociallinks"];
+
 
 ?>
 
@@ -47,184 +45,187 @@ $sociallinks = $_POST["sociallinks"];
 
 </head>
 
-<body>
-
-<header>
-    <?php
-    include "header.html";
-    ?>
-</header>
-
-<div>
-    <h1>Bearbeite dein Profil</h1>
-    <form action="ei"></form>
-
-   <div class="w3-sidebar w3-light-grey w3-bar-block" style="width: 25%">
-            <h3>Info</h3>
-
-            <li><a href="#" class="active" id="button1">Übersicht</a></li>
-
-            <li><a href="#" id="button2">Ich an der HdM</a></li>
-
-            <li><a href="#" id="button3">Arbeit und Ausbildung</a></li>
-
-            <li><a href="#" id="button4">Kontaktinformationen</a></li>
-        </div>
-
-    <div id="upload" div style=>
-    <div id="upload"  style=>
-        <form action="" method="POST">
-            Bild: <input type="file" name="bild">
-            <input type="submit" name="upload" value="upload">
-        </form>
-    </div>
-
-
-<h1>Übersicht</h1>
-<form action="do_Übergabe_Benutzerprofil.php" id= "uebersicht" method="post">
-    <p>Vorname<input type="text" id="vorname" name="vorname"></p>
-    <p>Name<input type="text" id="name" name="name"></p>
-    <p>E-Mail<input type="text" id="email" name="email"></p>
-    <p><button type="submit" name="submit">Speichern</button></p>
-</form>
-
-
-<h1>Übersicht</h1>
-
+<!--<body>-->
+<!---->
+<!--<header>-->
+<!--    --><?php
+//    include "header.html";
+//    ?>
+<!--</header>-->
+<!---->
+<!--<div>-->
+<!--    <h1>Bearbeite dein Profil</h1>-->
+<!--    <form action="ei"></form>-->
+<!---->
+<!--   <div class="w3-sidebar w3-light-grey w3-bar-block" style="width: 25%">-->
+<!--            <h3>Info</h3>-->
+<!---->
+<!--            <li><a href="#" class="active" id="button1">Übersicht</a></li>-->
+<!---->
+<!--            <li><a href="#" id="button2">Ich an der HdM</a></li>-->
+<!---->
+<!--            <li><a href="#" id="button3">Arbeit und Ausbildung</a></li>-->
+<!---->
+<!--            <li><a href="#" id="button4">Kontaktinformationen</a></li>-->
+<!--        </div>-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--    <div id="upload" div style=>-->
+<!--    <div id="upload"  style=>-->
+<!--        <form action="" method="POST">-->
+<!--            Bild: <input type="file" name="bild">-->
+<!--            <input type="submit" name="upload" value="upload">-->
+<!--        </form>-->
+<!--    </div>-->
+<!---->
+<!---->
+<!--<h1>Übersicht</h1>-->
+<!--<form action="do_Übergabe_Benutzerprofil.php" method="post">-->
+<!--    <p>Vorname<input type="text" id="vorname" name="vorname"></p>-->
+<!--    <p>Name<input type="text" id="name" name="name"></p>-->
+<!--    <p>E-Mail<input type="text" id="email" name="email"></p>-->
+<!--    <p><button type="submit" name="submit">Speichern</button></p>-->
+<!--</form>-->
+<!---->
+<!---->
+<!--<h1>Übersicht</h1>-->
+<!---->
 <?php
-$pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
-$sql = "SELECT vorname, nachname, email from users WHERE id=28";
-$stmt= $pdo->prepare($sql);
-$stmt->execute();
-while ($zeile = $stmt -> fetchObject()) {
-    var_dump($zeile);
-}
-?>
-
-    <h1>Ich an der HdM</h1>
-    <form action="do_Übergabe_Benutzerprofil.php" method="post">
-        <p>Fakultät:<input type="dropdown" id="fakultät" name="fakultät"></p>
-        <p>Studiengang:<input type="dropdown" id="studiengang" name="studiengang"></p>
-        <p>Semester:<input type="text" id="semester" name="semester"></p>
-        <p>Laufende Projekte:<input type="text" id="projekte" name="projekte"></p>
-        <p><button type="submit" name="submit">Speichern</button></p>
-    </form>
-
-
-    <h1>Ich an der HdM</h1>
-
-    <?php
-    $pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
-    $sql = "Select fakultät, studiengang, semester, projekte from users where id=28";
-    $stmt= $pdo->prepare($sql);
-    $stmt->execute();
-    while ($zeile = $stmt -> fetchObject()) {
-        echo "$zeile->fakultät";
-        echo "$zeile->studiengang";
-        echo "$zeile->semester";
-        echo "$zeile->projekte";
-    }
-
-    $sql=$pdo->prepare("SELECT 'fakultät' FROM Fakultät");
-    $sql->execute();
-    while($sql=$sql->fetch(PDO::FETCH_ASSOC)) {
-        echo "<option value=/" . $sql["fakultät"] . '">' . $sql["fakultät"];
-    }
-    ?>
-
-
-
-
-    <h1>Arbeit und Ausbildung</h1>
-    <form action="do_Übergabe_Benutzerprofil.php" method="post">
-        <p>Ist hier zur Schule gegangen:<input type="text" id="schule" name="schule"></p>
-        <p>Arbeitet aktuell bei:<input type="text" id="arbeit" name="arbeit"></p>
-        <p><button type="submit" name="submit">Speichern</button></p>
-    </form>
-
-
-    <h1>Arbeit und Ausbildung</h1>
-
-    <?php
-    $pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
-    $sql = "Select schule, arbeit, from users where id=28";
-    $stmt= $pdo->prepare($sql);
-    $stmt->execute();
-    while ($zeile = $stmt -> fetchObject()) {
-        echo "$zeile->schule";
-        echo "$zeile->arbeit";
-    }
-    ?>
-
-
-    <h1>Hier findest du mich auch</h1>
-    <form action="do_Übergabe_Benutzerprofil.php" method="post">
-        <p>Websites<input type="text" id="websites" name="vorname"></p>
-        <p>Social Links<input type="text" id="sociallinks" name="name"></p>
-        <p><button type="submit" name="submit">Speichern</button></p>
-    </form>
-
-
-    <h1>Hier findest du mich auch</h1>
-
-    <?php
-    $pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
-    $sql = "Select websites, sociallink, from users where id=28";
-    $stmt= $pdo->prepare($sql);
-    $stmt->execute();
-    while ($zeile = $stmt -> fetchObject()) {
-        echo "$zeile->websites";
-        echo "$zeile->sociallinks";
-
-    }
-    ?>
-
-
-<script>
-
-
-    $(document).ready(function () {
-        $('#ArbeitundAusbildung').hide();
-        $('#IchanderHdM').hide();
-        $('#Uebersicht').show();
-        $('#Hierfindestdumichauch').hide();
-    });
-
-    $(function () {
-        $('#button1').click(function () {
-            $('#IchanderHdM').hide();
-            $('#ArbeitundAusbildung').hide();
-            $('#Uebersicht').show();
-            $('#Hierfindestdumichauch').hide();
-        });
-    });
-
-    $(function () {
-        $('#button2').click(function () {
-            $('#IchanderHdM').show();
-            $('#ArbeitundAusbildung').hide();
-            $('#Uebersicht').hide();
-            $('#Hierfindestdumichauch').hide();
-        });
-    });
-
-    $(function () {
-        $('#button3').click(function () {
-            $('#IchanderHdM').hide();
-            $('#ArbeitundAusbildung').show();
-            $('#Uebersicht').hide();
-            $('#Hierfindestdumichauch').hide();
-        });
-    });
-
-    $(function () {
-        $('#button4').click(function () {
-            $('#IchanderHdM').hide();
-            $('#ArbeitundAusbildung').hide();
-            $('#Uebersicht').hide();
-            $('#Hierfindestdumichauch').show();
-        });
-    });
-</script>
-</body>
-</html>
+//$pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
+//$sql = "SELECT vorname, nachname, email from users WHERE id=28";
+//$stmt= $pdo->prepare($sql);
+//$stmt->execute();
+//while ($zeile = $stmt -> fetchObject()) {
+//    var_dump($zeile);
+//}
+//?>
+<!---->
+<!--    <h1>Ich an der HdM</h1>-->
+<!--    <form action="do_Übergabe_Benutzerprofil.php" method="post">-->
+<!--        <p>Fakultät:<input type="dropdown" id="fakultät" name="fakultät"></p>-->
+<!--        <p>Studiengang:<input type="dropdown" id="studiengang" name="studiengang"></p>-->
+<!--        <p>Semester:<input type="text" id="semester" name="semester"></p>-->
+<!--        <p>Laufende Projekte:<input type="text" id="projekte" name="projekte"></p>-->
+<!--        <p><button type="submit" name="submit">Speichern</button></p>-->
+<!--    </form>-->
+<!---->
+<!---->
+<!--    <h1>Ich an der HdM</h1>-->
+<!---->
+<!--    --><?php
+//    $pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
+//    $sql = "Select fakultät, studiengang, semester, projekte from users where id=28";
+//    $stmt= $pdo->prepare($sql);
+//    $stmt->execute();
+//    while ($zeile = $stmt -> fetchObject()) {
+//        echo "$zeile->fakultät";
+//        echo "$zeile->studiengang";
+//        echo "$zeile->semester";
+//        echo "$zeile->projekte";
+//    }
+//
+//    $sql=$pdo->prepare("SELECT 'fakultät' FROM Fakultät");
+//    $sql->execute();
+//    while($sql=$sql->fetch(PDO::FETCH_ASSOC)) {
+//        echo "<option value=/" . $sql["fakultät"] . '">' . $sql["fakultät"];
+//    }
+//    ?>
+<!---->
+<!---->
+<!---->
+<!---->
+<!--    <h1>Arbeit und Ausbildung</h1>-->
+<!--    <form action="do_Übergabe_Benutzerprofil.php" method="post">-->
+<!--        <p>Ist hier zur Schule gegangen:<input type="text" id="schule" name="schule"></p>-->
+<!--        <p>Arbeitet aktuell bei:<input type="text" id="arbeit" name="arbeit"></p>-->
+<!--        <p><button type="submit" name="submit">Speichern</button></p>-->
+<!--    </form>-->
+<!---->
+<!---->
+<!--    <h1>Arbeit und Ausbildung</h1>-->
+<!---->
+<!--    --><?php
+//    $pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
+//    $sql = "Select schule, arbeit, from users where id=28";
+//    $stmt= $pdo->prepare($sql);
+//    $stmt->execute();
+//    while ($zeile = $stmt -> fetchObject()) {
+//        echo "$zeile->schule";
+//        echo "$zeile->arbeit";
+//    }
+//    ?>
+<!---->
+<!---->
+<!--    <h1>Hier findest du mich auch</h1>-->
+<!--    <form action="do_Übergabe_Benutzerprofil.php" method="post">-->
+<!--        <p>Websites<input type="text" id="websites" name="vorname"></p>-->
+<!--        <p>Social Links<input type="text" id="sociallinks" name="name"></p>-->
+<!--        <p><button type="submit" name="submit">Speichern</button></p>-->
+<!--    </form>-->
+<!---->
+<!---->
+<!--    <h1>Hier findest du mich auch</h1>-->
+<!---->
+<!--    --><?php
+//    $pdo = new PDO ($dsn, $dbuser, $dbpass, array('charset'=>'utf8'));
+//    $sql = "Select websites, sociallink, from users where id=28";
+//    $stmt= $pdo->prepare($sql);
+//    $stmt->execute();
+//    while ($zeile = $stmt -> fetchObject()) {
+//        echo "$zeile->websites";
+//        echo "$zeile->sociallinks";
+//
+//    }
+//    ?>
+<!---->
+<!---->
+<!--<script>-->
+<!---->
+<!---->
+<!--    $(document).ready(function () {-->
+<!--        $('#ArbeitundAusbildung').hide();-->
+<!--        $('#IchanderHdM').hide();-->
+<!--        $('#Uebersicht').show();-->
+<!--        $('#Hierfindestdumichauch').hide();-->
+<!--    });-->
+<!---->
+<!--    $(function () {-->
+<!--        $('#button1').click(function () {-->
+<!--            $('#IchanderHdM').hide();-->
+<!--            $('#ArbeitundAusbildung').hide();-->
+<!--            $('#Uebersicht').show();-->
+<!--            $('#Hierfindestdumichauch').hide();-->
+<!--        });-->
+<!--    });-->
+<!---->
+<!--    $(function () {-->
+<!--        $('#button2').click(function () {-->
+<!--            $('#IchanderHdM').show();-->
+<!--            $('#ArbeitundAusbildung').hide();-->
+<!--            $('#Uebersicht').hide();-->
+<!--            $('#Hierfindestdumichauch').hide();-->
+<!--        });-->
+<!--    });-->
+<!---->
+<!--    $(function () {-->
+<!--        $('#button3').click(function () {-->
+<!--            $('#IchanderHdM').hide();-->
+<!--            $('#ArbeitundAusbildung').show();-->
+<!--            $('#Uebersicht').hide();-->
+<!--            $('#Hierfindestdumichauch').hide();-->
+<!--        });-->
+<!--    });-->
+<!---->
+<!--    $(function () {-->
+<!--        $('#button4').click(function () {-->
+<!--            $('#IchanderHdM').hide();-->
+<!--            $('#ArbeitundAusbildung').hide();-->
+<!--            $('#Uebersicht').hide();-->
+<!--            $('#Hierfindestdumichauch').show();-->
+<!--        });-->
+<!--    });-->
+<!--</script>-->
+<!--</body>-->
+<!--</html>-->
--- do_Übergabe_Benutzerprofil.php
+++ do_Übergabe_Benutzerprofil.php
@@ -1,9 +1,63 @@
 <?php
 session_start();
-$id = $_SESSION['id'];
-echo $id;
+if(!isset($_SESSION["username"]))
+{
+    echo"Bitte zuerst <a href=\"login.html\">einloggen</a>";
+    die();
+}
+else {
+    $eingeloggt = $_SESSION['username'];
+    echo "Hallo User: ".$eingeloggt;
+}
 
-include_once ('./passwords/db.php');
+if(isset($_POST["Update"]))
+{
+    $vorname = $_POST["vorname"];
+    $nachname = $_POST["nachname"];
+    $email = $_POST["email"];
+
+    $fakultät = $_POST["fakultät"];
+    $studiengang = $_POST["studiengang"];
+
+    $semester = $_POST["semester"];
+
+    $arbeitgeber = $_POST["arbeitgeber"];
+
+}
+
+else
+{
+    echo"Keine Daten";
+    die();
+}
+
+
+$pdo = new PDO($dsn, $dbuser, $dbpass, $option);
+
+$statement = $pdo->prepare("UPDATE users SET vorname=:vorname , nachname=:nachname , email=:email , fakultät=:fakultät , studiengang=:studiengang , semester=:semester , arbeitgeber=:arbeitgeber WHERE id=:userid");
+
+$ergebnis = $statement->execute(array( ':userid'=>$eingeloggt, ':vorname'=>$vorname , ':nachname'=>$nachname , ':email'=>$email, ':fakultät'=>$fakultät , ':studiengang'=>$studiengang , ':semester'=>$semester , ':arbeitgeber'=>$arbeitgeber  ));
+
+
+if($ergebnis) {
+    echo 'Du hast erfolgreich deine Angaben geändert. <a href="Benutzerprofil(1).php">Weiter zum Profil</a>';
+}
+else {
+    echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+    echo "Datenbank-Fehler:";
+    echo $statement->errorInfo()[2];
+    echo $statement->queryString;
+    die();
+}
+
+
+
+
+
+
+
+/*include_once ('./passwords/db.php');
+if (isset ($_POST ["vorname"]) AND isset ($_POST ["nachname"]) AND isset ($_POST ["email"])  AND isset ($_POST ["fakultät"]) AND isset ($_POST ["studiengang"]) AND isset ($_POST ["semester"]) AND isset  ($_POST ["projekte"] AND isset ($_POST ["arbeitgeber"]) AND isset ($_POST ["websites"]);
 
 $vorname = $_POST["vorname"];
 $nachname = $_POST["nachname"];
@@ -17,11 +71,8 @@ $semester = $_POST["semester"];
 $projekte = $_POST["projekte"];
 
 $arbeitgeber = $_POST["arbeitgeber"];
-$schule = $_POST["schule"];
-$seit = $_POST["seit"];
 
 $websites = $_POST["websites"];
-$sociallinks = $_POST["sociallinks"];
 
 
 $pdo = new PDO($dsn, $dbuser, $dbpass, $option);
@@ -31,6 +82,6 @@ $statement->execute(array( "$vorname", "$nachname", "$email"));
 $row = $statement->fetchObject();
 
 header("Location: Benutzerprofil(1).php");
-?>
+*/?><!--
 
-
+-->
