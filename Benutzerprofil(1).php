ae865b23764e4c19f2be4b2d4fb89551c80b75f5
niklasehinger <niklasehinger@googlemail.com>
2018-12-04 15:56:14 +0100

    hey

--- Benutzerprofil(1).php
+++ Benutzerprofil(1).php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 if(!isset($_SESSION['username'])) {
-    echo "kuzfk";
-    echo"Bitte zuerst <a href=\"login.php\">einloggen</a>";
+    echo "Fehler";
+    echo"Bitte zuerst <a href=\"start.php\">einloggen</a>";
     die();
 }
 
