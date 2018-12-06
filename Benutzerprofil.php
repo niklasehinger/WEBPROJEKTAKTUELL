d1d2a49d4b15a90f367405dde22a32084387a823
niklasehinger <niklasehinger@googlemail.com>
2018-12-06 21:42:25 +0100

    hey

--- Benutzerprofil.php
+++ Benutzerprofil.php
@@ -29,12 +29,48 @@ if(!isset($_SESSION['username'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Mein Profil</title>
+
+    <style>
+        #update_benutzerprofil {
+            width: 30%;
+            margin: 40px auto;
+            padding: 20px;
+            border: 1px solid #B0C4DE;
+            background: white;
+            border-radius: 0px 0px 10px 10px;
+        }
+
+        .input-group {
+            margin: 10px 0px 10px 0px;
+        }
+        .input-group label {
+            display: block;
+            text-align: left;
+            margin: 3px;
+        }
+        .input-group input {
+            height: 30px;
+            width: 93%;
+            padding: 5px 10px;
+            font-size: 16px;
+            border-radius: 5px;
+            border: 1px solid gray;
+        }
+        .btn {
+            padding: 10px;
+            font-size: 15px;
+            color: white;
+            background: #000000;
+            border: none;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 
 <body>
 
 <div id="container" align="center" style="width:100%; height:100%">
-   <h1>Deine Daten</h1>
+   <h1> Deine Daten</h1>
     <div class="profil" align="center" style="width: 50%; height: 50%; background-color: #2b4046; color: white; margin-top: 10px; padding-top: 5px">
         <?php
         $pdo = new PDO($dsn, $dbuser, $dbpass, $option);
@@ -53,35 +89,52 @@ if(!isset($_SESSION['username'])) {
             echo "E-Mail: ".$row['email']."<br /><br />";
         }
         ?>
-        <button type="submit"><img src="pictures/icons/cogwheel-setting-2.png"> Text</button>
+        <button type="submit"><img src="pictures/icons/cogwheel-setting-2.png" id="updatebutton" align="top"></button>
     </div>
-    <div class="update_benutzerprofil">
-        <form class="update_benutzerprofil" method="post" action="phpfiles/do_update_Benutzerprofil.php">
+
+
+        <form id="update_benutzerprofil" method="post" action="phpfiles/do_update_Benutzerprofil.php">
             <div class="input-group">
                 <label>Vorname</label>
                 <input type="text" name="vorname" required>
             </div>
             <div class="input-group">
                 <label>Nachname</label>
-                <input type="password" name="nachname" required>
+                <input type="text" name="nachname" required>
             </div>
             <div class="input-group">
                 <label>Studiengang</label>
-                <input type="password" name="studiengang" required>
+                <input type="text" name="studiengang" required>
             </div>
             <div class="input-group">
                 <label>Email</label>
-                <input type="password" name="email" required>
+                <input type="email" name="email" required>
             </div>
             <div class="input-group">
                 <button type="submit" class="btn" name="update_user">Update</button>
             </div>
         </form>
-    </div>
+
 </div>
 
 
 
 
 </body>
+<script>
+    $(document).ready (function(){
+        $('#update_benutzerprofil').hide();
+    });
+
+    $(function () {
+        $('#updatebutton').click(function () {
+            $('#update_benutzerprofil').show();
+
+
+        });
+    });
+
+
+</script>
+
 </html>
--- phpfiles/do_update_Benutzerprofil.php
+++ phpfiles/do_update_Benutzerprofil.php
@@ -3,14 +3,20 @@
 include '../passwords/db.php';
 session_start();
 
-$post = $_POST["post"];
+$vorname = $_POST["vorname"];
+$nachname = $_POST["nachname"];
+$studiengang = $_POST["studiengang"];
+$email = $_POST["email"];
 $username = $_SESSION["username"];
 
-$pdo = new PDO ($dsn, $dbuser, $dbpass, $option);
-$sql = "INSERT INTO posts (author, content) VALUES (?, ?)";
-$statement = $pdo->prepare($sql);
-$statement->execute(array("$username", "$post"));
+
+$pdo = new PDO($dsn, $dbuser, $dbpass, $option);
+
+$statement = $pdo->prepare("UPDATE users SET vorname = :vorname_neu, email = :email_neu, nachname = :nachname_neu, studiengang = :studiengang_neu WHERE username = :username");
+$statement->execute(array('username' => $username, 'email_neu' => $email, 'vorname_neu' => $vorname, 'nachname_neu' => $nachname, 'studiengang_neu' => $studiengang));
+
+header("Location: ../Benutzerprofil.php");
+?>
 
 
-header("Location: ../index.php");
 
