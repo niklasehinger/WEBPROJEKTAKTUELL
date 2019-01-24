333dc143a5730bb448e6389f2bf03b0690adef3b
Tim <tr086@hdm-stuttgart.de>
2019-01-24 22:28:15 +0100

    Header push

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 session_start();
 include 'passwords/db.php';
 
-$username=$_SESSION['username'];
+$username = $_SESSION['username'];
 
 if (!isset($_SESSION['username'])) {
     echo "
@@ -37,8 +37,7 @@ if (!isset($_SESSION['username'])) {
                 </a>
                 </div>
             </div>   
-             "
-    ;
+             ";
     die();
 }
 
@@ -52,10 +51,17 @@ if (!isset($_SESSION['username'])) {
     <title>Pigeon</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+            integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+            crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="sweetalert2.all.min.js"></script>
     <!-- Optional: include a polyfill for ES6 Promises for IE11 and Android browser -->
@@ -68,7 +74,7 @@ if (!isset($_SESSION['username'])) {
     <link rel="stylesheet" type="text/css" href="style.css">
     <style>
 
-        div .content{
+        div .content {
             height: max-content;
         }
 
@@ -84,8 +90,8 @@ if (!isset($_SESSION['username'])) {
             background-size: cover;
         }
 
-        .farbe{
-           color: #0068ff;
+        .farbe {
+            color: whitesmoke;
         }
 
 
@@ -93,113 +99,118 @@ if (!isset($_SESSION['username'])) {
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-white bg-white">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Navigation ein-/ausblenden</span>
-                <span class="icon-bar" style="background-color: #0068ff;"></span>
-                <span class="icon-bar" style="background-color: #0068ff;"></span>
-                <span class="icon-bar" style="background-color: #0068ff;"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">
-                <img alt="Logo" src="Logos/Logo-Blau.png" style="display:inline" width="30" height="30">
+<nav class="navbar navbar-expand-lg sticky-top navbar-white " style="background-color: whitesmoke;>
+    <div class=" container-fluid
+">
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Navigation ein-/ausblenden</span>
+        <span class="icon-bar" style="background-color: #0068ff;"></span>
+        <span class="icon-bar" style="background-color: #0068ff;"></span>
+        <span class="icon-bar" style="background-color: #0068ff;"></span>
+    </button>
+    <a href="index.php">
+        <img alt="Logo" src="Logos/Logo-Blau.png" style="display:inline" width="40" height="40">
+    </a>
+</div>
+
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+        <li class="nav-item dropdown">
+            <a class="nav-link" href="http://example.com" id="dropdown01" style="color: #0068ff"
+               data-toggle="dropdown"
+               aria-haspopup="true" aria-expanded="false">Benachrichtigungen
+                <?php //hier werden die ungelesenen nachrichten ausgelesen
+                $anzahl = 0;
+                $pdo = new PDO($dsn, $dbuser, $dbpass, $option);
+
+                $statement = $pdo->prepare("SELECT * FROM posts WHERE gelesen='0' AND author = ANY (SELECT usernameandere from following WHERE username=:username)"); //nimm alls spalten aus der tabelle null wo gelesen auf null gessetzt ist, also die posts die noch ungelesen sind
+                $statement->execute(array(":username" => "$username"));
+                $anzahl = $statement->rowCount(); //zähle die zeilen in der tabelle wo er NULL findet und zeige die anzahl der spalten als anzahl der benachrichtigungen an
+
+                ?>
+                <span class="badge badge-primary"><?php echo $anzahl ?></span>
+                <!--er soll die variable an dieser stelle ausgeben-->
             </a>
-        </div>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="http://example.com" id="dropdown01" data-toggle="dropdown"
-                       aria-haspopup="true" aria-expanded="false">Benachrichtigungen
-                        <?php //hier werden die ungelesenen nachrichten ausgelesen
-                        $anzahl = 0;
-                        $pdo = new PDO($dsn, $dbuser, $dbpass, $option);
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
 
-                        $statement = $pdo->prepare("SELECT * FROM posts WHERE gelesen='0' AND author = ANY (SELECT usernameandere from following WHERE username=:username)"); //nimm alls spalten aus der tabelle null wo gelesen auf null gessetzt ist, also die posts die noch ungelesen sind
-                        $statement->execute(array(":username"=>"$username"));
-                        $anzahl = $statement->rowCount(); //zähle die zeilen in der tabelle wo er NULL findet und zeige die anzahl der spalten als anzahl der benachrichtigungen an
+                <?php // wenn es Nachrichten gibt, dann zeige Klasse 'dropdown-item', ansonsten führe else aus 'Keine neuen Nachrichten'
 
-                        ?>
-                        <span class="badge badge-primary"><?php echo $anzahl ?></span>
-                        <!--er soll die variable an dieser stelle ausgeben-->
-                    </a>
+                if ($anzahl > 0) {
 
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    $username = $_SESSION['username'];
 
-                        <?php // wenn es Nachrichten gibt, dann zeige Klasse 'dropdown-item', ansonsten führe else aus 'Keine neuen Nachrichten'
+                    $sql = "SELECT * from posts WHERE gelesen='0' AND author = ANY (SELECT usernameandere from following WHERE username=:username)";
+                    $query = $pdo->prepare($sql);
+                    $query->execute(array(":username" => "$username"));
+                    $row = array();
+                    while ($row = $query->fetch()) {
 
-                        if ($anzahl > 0){
+                        //Beitrag wird als Listenelemt für das Dropdown in der Navbar zurückgegeben
 
-                        $username=$_SESSION['username'];
+                        echo '<a href="#">';
+                        echo '<ul>';
+                        echo '<li>';
+                        echo '<a href="do_gelesen.php?usernameandere=' . $row["author"] . '&id=' . $row["id"] . '">Neuer Beitrag von ' . $row["author"] . '</a>';
+                        echo '</li>';
+                        echo '</ul>';
+                        echo '</a>';
+                        echo '<div class="dropdown-divider"></div>';
 
-                        $sql = "SELECT * from posts WHERE gelesen='0' AND author = ANY (SELECT usernameandere from following WHERE username=:username)";
-                        $query = $pdo->prepare($sql);
-                        $query->execute(array(":username"=>"$username"));
-                        $row = array();
-                        while ($row = $query->fetch()) {
+                    }
+                } else {
+                    echo 'Keine neuen Nachrichten';
+                }
 
-                            //Beitrag wird als Listenelemt für das Dropdown in der Navbar zurückgegeben
-
-                            echo '<a href="#">';
-                            echo '<ul>';
-                            echo '<li>';
-                            echo '<a href="do_gelesen.php?usernameandere='.$row["author"].'&id='.$row["id"].'">Neuer Beitrag von '. $row["author"].'</a>';
-                            echo '</li>';
-                            echo '</ul>';
-                            echo '</a>';
-                            echo '<div class="dropdown-divider"></div>';
-
-                        }
-                        }
-                        else {
-                            echo 'Keine neuen Nachrichten';
-                        }
-
-                        ?>
-                    </div>
-                </li>
-
-                <li class="nav-item">
-                    <div class="headerprofilbild">
-                        <a href="Benutzerprofil.php">
-                            <?php
-
-                            $statement = $pdo->prepare("SELECT pb FROM users WHERE username = :username");
-                            $statement->execute(array(":username"=>"$username"));
-                            $query = $pdo->prepare($sql);
-                            $query -> execute();
-
-                            $profilbild = $row['pb'];
-
-
-                            if (file_exists("<img src='profilbild/$profilbild'>")) {
-                                echo "<img src=\"profilbild/$profilbild\" width=\"39\" height=\"39\" alt=\"\">";
-                            } else {
-                                echo "<img src=\"profilbild/root.jpg\" width=\"39\" height=\"39\" alt=\"\">";
-                            }
-                            ?>
-                        </a>
-                    </div>
-                </li>
-            </ul>
-
-
-
+                ?>
+            </div>
+        </li>
+        <di
+        <li>
             <form class="nav navbar-form navbar-right" method="get" action="do_suchen.php" role="search">
                 <div class="form-group">
-                    <input type="search" class="form-control" placeholder="Suchen" aria-label="Search" name="searchbox">
+                    <input type="search" class="form-control" placeholder="Suchen" aria-label="Search"
+                           name="searchbox">
                 </div>
                 <button type="submit" class="btn btn-default">Los</button>
             </form>
+        </li>
+
+        <li>
+            <div class="headerprofilbild style">
+                <a href="Benutzerprofil.php navbar-brand">
+                    <?php
+
+<<<<<<< Updated upstream
+=======
+                    $statement = $pdo->prepare("SELECT pb FROM users WHERE username = :username");
+                    $statement->execute(array(":username" => "$username"));
+                    $query = $pdo->prepare($sql);
+                    $query->execute();
+
+                    $profilbild = $row['pb'];
 
 
-            <form class="navbar-form navbar-right" method="post" action="phpfiles/logout.php" role="logout">
+                    if (file_exists("<img src='profilbild/$profilbild'>")) {
+                        echo "<img class=\"img-circle\" src=\"profilbild/$profilbild\" style=\"padding: 5px\" width=\"50\" height=\"50\" alt=\"\">";
+                    } else {
+                        echo "<img class=\"img-circle\" src=\"profilbild/root.jpg\" style=\"padding: 5px\" width=\"50\" height=\"50\" alt=\"\">";
+                    }
+                    ?>
+                </a>
+            </div>
+        </li>
+
+        <li>
+>>>>>>> Stashed changes
+
+            <form class="navbar-form " method="post" style="margin-left: -15px" action="phpfiles/logout.php">
                 <button type="submit" class="btn btn-default">Logout</button>
             </form>
-        </div>
-    </div>
-    </div>
+        </li>
+    </ul>
+</div>
 </nav>
 
