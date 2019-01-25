ec575a957d0ce9cdc5a171f0b861bdd72d7ea320
Tim <tr086@hdm-stuttgart.de>
2019-01-25 02:23:26 +0100

    new

--- header.php
+++ header.php
@@ -70,46 +70,85 @@ if (!isset($_SESSION['username'])) {
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
         <style>
-
-
         </style>
-
     </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php">
+        <img src="Logos/Logo-Blau.png" alt="logo" style="width:40px;">
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                <a class="navbar-brand" href="#">
+                    <img src="Logos/Logo-Text-SW.png" alt="Profilbild" style="width:40px;">
                 </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        <ul class="navbar-nav my-2 my-lg-0">
+            <li class="nav-item dropdown">
+                <a class="nav-link" href="http://example.com" id="dropdown01" style="color: #0068ff"
+                   data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">Benachrichtigungen
+                    <?php //hier werden die ungelesenen nachrichten ausgelesen
+                    $anzahl = 0;
+                    $pdo = new PDO($dsn, $dbuser, $dbpass, $option);
+
+                    $statement = $pdo->prepare("SELECT * FROM posts WHERE gelesen='0' AND author = ANY (SELECT usernameandere from following WHERE username=:username)"); //nimm alls spalten aus der tabelle null wo gelesen auf null gessetzt ist, also die posts die noch ungelesen sind
+                    $statement->execute(array(":username" => "$username"));
+                    $anzahl = $statement->rowCount(); //zähle die zeilen in der tabelle wo er NULL findet und zeige die anzahl der spalten als anzahl der benachrichtigungen an
+
+                    ?>
+                    <span class="badge badge-primary"><?php echo $anzahl ?></span>
+                    <!--er soll die variable an dieser stelle ausgeben-->
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+
+                    <?php // wenn es Nachrichten gibt, dann zeige Klasse 'dropdown-item', ansonsten führe else aus 'Keine neuen Nachrichten'
+
+                    if ($anzahl > 0) {
+
+                        $username = $_SESSION['username'];
+
+                        $sql = "SELECT * from posts WHERE gelesen='0' AND author = ANY (SELECT usernameandere from following WHERE username=:username)";
+                        $query = $pdo->prepare($sql);
+                        $query->execute(array(":username" => "$username"));
+                        $row = array();
+                        while ($row = $query->fetch()) {
+
+                            //Beitrag wird als Listenelemt für das Dropdown in der Navbar zurückgegeben
+
+                            echo '<a href="#">';
+                            echo '<ul>';
+                            echo '<li>';
+                            echo '<a href="do_gelesen.php?usernameandere=' . $row["author"] . '&id=' . $row["id"] . '">Neuer Beitrag von ' . $row["author"] . '</a>';
+                            echo '</li>';
+                            echo '</ul>';
+                            echo '</a>';
+                            echo '<div class="dropdown-divider"></div>';
+
+                        }
+                    } else {
+                        echo 'Keine neuen Nachrichten';
+                    }
+
+                    ?>
+                </div>
+            </li>
+            <form class="form-inline" action="do_suchen.php" method="get">
+                <input class="form-control mr-sm-2" type="search" placeholder="Suchen" name="searchbox" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Los!</button>
+            </form>
+            <form class="form-inline" method="post" action="phpfiles/logout.php">
+                <button type="submit" class="btn btn-default">Logout</button>
+            </form>
+        </ul>
     </div>
 </nav>
 
