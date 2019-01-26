ce4e6c84d6c14c9a0fc94eea213f153a8ee398a2
Tim <tr086@hdm-stuttgart.de>
2019-01-26 21:54:04 +0100

    error login

--- error_einloggen.php
+++ error_einloggen.php
@@ -1,41 +1,44 @@
 <?php
+session_start();
+include 'passwords/db.php';
+
 $username = $_SESSION['username'];
 
 if (!isset($_SESSION['username'])) {
     echo "
 
-<html lang=\"de\" xmlns=\"http://www.w3.org/1999/html\">
+<html lang=\"de\">
 <title>Pigeon</title>
 <head>
-    
-    <meta charset=\"utf-8\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, shrink-to-fit=no\">
-    <script src=\"https://unpkg.com/sweetalert/dist/sweetalert.min.js\"></script>
-    <script src=\"../ui/sweetalert/sweetalert2.min.js\"></script>
-    <script src=\"https://code.jquery.com/jquery-3.3.1.min.js\"></script>
-    <link rel=\"stylesheet\" href=\"../ui/sweetalert/sweetalert2.min.css\">
-    <link rel=\"stylesheet\" href=\"htteps://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">
-    <link href=\"https://fonts.googleapis.com/css?family=Roboto:400,500,700,700i\" rel=\"stylesheet\">
-    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
-    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">
     <style>
     .shadow {
-    background-image: url("Logos/");
-    background-color: whitesmoke;
-    border-radius: 100px ;
-    box-shadow:1px 1px 30px 1px  lightgrey;
+    background-color: white;
+    border-radius: 300px ;
+    box-shadow:1px 1px 30px 1px  darkblue;
+     padding:50px
+}
+
+    body {
+    padding-top: 5rem;
+    background: url('Logos/BG.jpg');
+    background-size: cover;
+    font: 14px Roboto, sans-serif;
+    background-color: white;
+        }
 }
 </style>
 </head>
-    <div class=\"container\" style=\"margin-top: 200px\">
-        <div class='jumbatron'>    
+    <div class='container' style=\"margin-top: 50px \" align='center'>
+        <div class='marginmedia' align='center'>
            <a class=\"center-block btn btn-outline-dark center\" href=\"start.php\" align=\"center\" role=\"button\" >
-                    <img src=\"Logos/Pigeonlogo_einloggen.png\" href=\"start.php\" type=\"button\" class=\"img-responsive logo shadow\" style=\"display:inline\" alt=\"Logo\" width=\"350\" height=\"350\"> 
-                </a>
-                </div>
-            </div>   
+                    <img src=\"Logos/Logo-Text-Blau.png\" href=\"start.php\" type=\"button\" class=\"img-responsive shadow\" style=\"display:inline\" alt=\"Logo\" width=\"200\" height=\"200\"> 
+                    <h2 style='padding: 20px; color: white'>Bitte erst einloggen!</h2>
+           </a>
+        </div>   
+    </div>
+    
              ";
     die();
 }
+
 ?>
