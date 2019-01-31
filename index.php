6192b4c2d19954a6ba983ecb4e7fb4917d52c7e6
Tim <tr086@hdm-stuttgart.de>
2019-01-31 12:56:12 +0100

    Hallo was isch denn los hier?

--- index.php
+++ index.php
@@ -17,25 +17,27 @@ include 'header.php';
                     <img class="d-block w-100" src="Logos/hdm2.jpg" alt="Second slide">
                 </div>
                 <div class="feed" align="center" style="margin-top: 40%">
-                        <div class="create_post col-md-5" align="center">
-                            <form action="phpfiles/do_post.php" method="post">
-                            <textarea class="form-control form-rounded" rows="2" placeholder="Was gibts neues?" name="post"></textarea><br>
-                                <button type="submit" name="submit" class="btn btn-primary bildposten">Posten</button>
-                                <button type="button" class="btn btn-primary bildposten" data-toggle="modal" data-target=".bd-example-modal-sm">Bild posten</button>
-                            </form>
+                    <div class="create_post col-md-5" align="center">
+                        <form action="phpfiles/do_post.php" method="post">
+                            <textarea class="form-control form-rounded" rows="2" placeholder="Was gibts neues?"
+                                      name="post"></textarea><br>
+                            <button type="submit" name="submit" class="btn btn-primary bildposten">Posten</button>
+                            <button type="button" class="btn btn-primary bildposten" data-toggle="modal"
+                                    data-target=".bd-example-modal-sm">Bild posten
+                            </button>
+                        </form>
 
-                            <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog"
-                                 aria-labelledby="mySmallModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-sm">
-                                    <div class="modal-content">
-                                        <div class="create_post margin">
-                                            <form action="phpfiles/do_bildupload.php" method="post"
-                                                  enctype="multipart/form-data">
-                                                <input type="file" name="file">
-                                                <input type="submit" value="Bild posten" name="submit"
-                                                       class="btn btn-primary">
-                                            </form>
-                                        </div>
+                        <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog"
+                             aria-labelledby="mySmallModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-sm">
+                                <div class="modal-content">
+                                    <div class="create_post margin">
+                                        <form action="phpfiles/do_bildupload.php" method="post"
+                                              enctype="multipart/form-data">
+                                            <input type="file" name="file">
+                                            <input type="submit" value="Bild posten" name="submit"
+                                                   class="btn btn-primary">
+                                        </form>
                                     </div>
                                 </div>
                             </div>
@@ -43,6 +45,7 @@ include 'header.php';
                     </div>
                 </div>
             </div>
+        </div>
     </header>
 </div>
 
@@ -64,32 +67,32 @@ include 'header.php';
 
         if ($row['content'] == NULL) {
 
-            echo " <div class=\"row featurette\" style='background-color: white'>
-                        <div class=\"col-md-7 order-md-2\" style='background-color: white'>
-                            <a href='bildupload/$bildlink' class=\"lead\"><img src='bildupload/$bildlink' width='200px' height='200px'></a>
+            echo "<div class=\"row featurette\" >
+                        <div class=\"col-md-9 order-md-2\" style='background-color: whitesmoke'>
+                            <a href='bildupload/$bildlink' class=\"lead float-left\"><img src='bildupload/$bildlink' width='200px' height='200px'></a>
                         </div>
-                    <div class=\"col-md-5 order-md-1\">
-                        <p class=\"lead\">" . $row['author'] . "</p>
+                    <div class=\"col-md-3 order-md-1 text-center\" style='background-color: darkgray'>
+                        <h2 class=\"lead\">" . $row['author'] . "</h2>
                         <a href=\"andererprofile.php?usernameandere=" . $row['author'] . "\">    
-                        <img class=\"content img-fluid mx-auto\" data-src=\"holder.js/500x500/auto\" alt=\"500x500\"
-                         src='profilbild/$pb'
-                         data-holder-rendered=\"true\" style=\"width: 200px; height: 200px;\">
+                            <img class=\"rounded-circle\" data-src=\"holder.js/500x500/auto\" alt=\"100x100\"
+                            src='profilbild/$pb'
+                            data-holder-rendered=\"true\" style=\"width: 100px; height: 100px;\">
                         </a>    
                     </div>
                     </div>
                 <br><br>
             ";
         } else {
-            echo " <div class=\"row featurette\" style='background-color: #0068ff'>
-                        <div class=\"col-md-7 order-md-2\" style='background-color: white'>
+            echo "<div class=\"row featurette\">
+                        <div class=\"col-md-9 order-md-2\" style='background-color: whitesmoke'>
                             <p class=\"lead\">" . $row['content'] . "</p>
                         </div>
-                    <div class=\"col-md-5 order-md-1\">
-                        <p class=\"lead\">" . $row['author'] . "</p>
+                    <div class=\"col-md-3 order-md-1 text-center\" style='background-color: darkgray'>
+                        <h2 class=\"lead\">" . $row['author'] . "</h2>
                         <a href=\"andererprofile.php?usernameandere=" . $row['author'] . "\">    
-                        <img class=\"content img-fluid mx-auto\" data-src=\"holder.js/500x500/auto\" alt=\"500x500\"
-                         src='profilbild/$pb'
-                         data-holder-rendered=\"true\" style=\"width: 200px; height: 200px;\">
+                            <img class=\"rounded-circle\" data-src=\"holder.js/500x500/auto\" alt=\"100x100\"
+                            src='profilbild/$pb'
+                            data-holder-rendered=\"true\" style=\"width: 100px; height: 100px;\">
                          </a>    
                     </div>
                     </div>
