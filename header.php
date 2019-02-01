d1e537569a83f276393e7d728c98f03121bbdab8
Tim <tr086@hdm-stuttgart.de>
2019-02-01 14:26:33 +0100

    new

--- header.php
+++ header.php
@@ -158,7 +158,7 @@ if (!isset($_SESSION['username'])) {
                     ?>
                 </a>
             </li>
-            <form class="form-inline" action="do_suchen.php" method="get">
+            <form class="form-inline" action="phpfiles/do_suchen.php" method="get">
                 <input class="form-control mr-sm-2" type="search" placeholder="Suchen" name="searchbox"
                        aria-label="Search">
                 <button class="btn btn-outline-primary my-2 my-sm-0" type="submit" style="color: #0068ff;">Los!</button>
